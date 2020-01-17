f98fa85e7adb5a1f7980aa4f43d29bf8d07c0e37
Salomon KABONGO <salomon@aims.ac.za>
2020-01-17 12:27:51 +0000

    Feature : Adding Helper Notebook

--- model.py
+++ model.py
@@ -2,24 +2,6 @@
 from torch import nn, optim
 import torch.nn.functional as F
 
-# class Classifier(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.fc1 = nn.Linear(150528, 256)
-#         self.fc2 = nn.Linear(256, 128)
-#         self.fc3 = nn.Linear(128, 64)
-#         self.fc4 = nn.Linear(64, 2)
-        
-#     def forward(self, x):
-#         # make sure input tensor is flattened
-#         x = x.view(x.shape[0], -1)
-        
-#         x = F.relu(self.fc1(x))
-#         x = F.relu(self.fc2(x))
-#         x = F.relu(self.fc3(x))
-#         x = F.log_softmax(self.fc4(x), dim=1)
-        
-#         return x
 
 class CNN_Classifier_1(nn.Module):
     def __init__(self, n_feature, output_size):
